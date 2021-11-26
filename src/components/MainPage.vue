204581b8b287b201c3893a0cae6434b2096ee237
Jason Scharf <jasonscharf3@gmail.com>
2021-11-26 11:54:45 -0500

    Added updated photo of Spotify App and updated text.

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -105,7 +105,7 @@
 
         <p>
           &bull; Created a web app allowing users to explore Spotify's API, using client side authentication, and RESTful API calls.  This allows the user to explore different musical artists based one a genre or artist they are interested in. <br />
-          &bull; Site can be explored at: 
+          &bull; Site can be visited at: 
           <a
             href="https://jason-spotify.netlify.app/"
             target="_blank"
