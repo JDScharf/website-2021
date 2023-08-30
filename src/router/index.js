7c48198ac5b3807c06b1593f515cfb543bfe36d4
Jason Scharf <43705075+JDScharf@users.noreply.github.com>
2023-08-29 21:26:27 -0400

    Hopefully fixing the data page link

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+// import Data from "../views/Data.vue";
 
 Vue.use(VueRouter);
 
@@ -17,7 +18,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "data" */ "../views/data.vue")
+      import(/* webpackChunkName: "data" */ "../views/Data.vue")
   }
 ];
 
