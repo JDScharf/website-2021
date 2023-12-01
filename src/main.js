4eb17c0bc12d1c29105f73c7a0fe3d213405e110
JDScharf <jasonscharf3@gmail.com>
2023-11-30 21:49:49 -0500

    Changed to using VueAnalytics

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false;
+
+// Configuration VueAnalytics
+Vue.use(VueAnalytics, {
+  id: 'G-HW1EQTLWH3'
+});
 
 new Vue({
   vuetify,
